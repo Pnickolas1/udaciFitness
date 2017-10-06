215ff416666dfd9887c6053c9f9c996ded5a8209
Pnickolas1 <petertountas1@gmail.com>
2017-10-05 21:20:38 -0500

    more updates

--- components/udaciStepper.js
+++ components/udaciStepper.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import { FontAwesome, Entypo } from '@expo/vector-icons'
+import { white, gray, purple } from '../utils/colors'
 
 
 export default function UdaciStepper({ max, unit, step, value, onIncrement, onDecrement }){
     return(
         <View style={[styles.row, {justifyContent: 'space-between'}]}>
             <View style={{flexDirection: 'row'}}>
-                <TouchableOpacity onPress={onDecrement}>
-                    <FontAwesome name="minus" size={30} color={'black'} />
+                <TouchableOpacity
+                     style={[styles.iosBtn,  {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}
+                     onPress={onDecrement}>
+                    <FontAwesome name="minus" size={30} color={purple} />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={onIncrement}>
-                    <FontAwesome name="plus" size={30} color={'black'} />
+                <TouchableOpacity 
+                    style={[styles.iosBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0} ]}
+                    onPress={onIncrement}>
+                    <FontAwesome name="plus" size={30} color={purple} />
                 </TouchableOpacity>
             </View>
-            <View>
-                <Text>{value}</Text>
-                <Text>{unit}</Text>
+            <View style={styles.metricCounter}>
+                <Text style={{ fontSize: 24, textAlign: 'center'}}>{value}</Text>
+                <Text style={{ fontSize: 18, color: gray}}>{unit}</Text>
             </View>
         </View>
     )
@@ -28,5 +33,19 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flex: 1, 
         alignItems: 'center'
+    },
+    iosBtn: {
+        backgroundColor: white,
+        borderColor: purple,
+        borderWidth: 1,
+        borderRadius: 3,
+        padding: 5, 
+        paddingLeft: 25,
+        paddingRight: 25,
+    },
+    metricCounter: {
+        width: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 })
